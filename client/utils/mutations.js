b049ca778fc5d2802bb5170bc30ee8a0d0f057f4
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-01 20:23:10 -0600

    added mutations for tasks

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -107,3 +107,84 @@ export const UPDATE_PROJECT = gql`
     }
   }
 `;
+
+export const ADD_TASK = gql`
+  mutation addTask(
+    $name: String!,
+    $comment: String,
+    $status: String!,
+    $dueDate: String,
+    $projectId: ID!,
+    $assignedTo: ID
+  ) {
+    addTask(
+      name: $name,
+      comment: $comment,
+      status: $status,
+      dueDate: $dueDate,
+      projectId: $projectId,
+      assignedTo: $assignedTo
+    ) {
+      _id
+      name
+      comment
+      status
+      dueDate
+      project {
+        _id
+        name
+      }
+      assignedTo {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const UPDATE_TASK = gql`
+  mutation updateTask(
+    $taskId: ID!,
+    $name: String,
+    $comment: String,
+    $status: String,
+    $dueDate: String,
+    $projectId: ID,
+    $assignedTo: ID
+  ) {
+    updateTask(
+      taskId: $taskId,
+      name: $name,
+      comment: $comment,
+      status: $status,
+      dueDate: $dueDate,
+      projectId: $projectId,
+      assignedTo: $assignedTo
+    ) {
+      _id
+      name
+      comment
+      status
+      dueDate
+      project {
+        _id
+        name
+      }
+      assignedTo {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const REMOVE_TASK = gql`
+  mutation removeTask($taskId: ID!) {
+    removeTask(taskId: $taskId) {
+      _id
+      name
+    }
+  }
+`;
