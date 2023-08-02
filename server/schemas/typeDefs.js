9f543f0f7ff0935ab0acfffb72f13c1e9053e9df
Cheryl L <cheryl.loeffler@gmail.com>
2023-08-01 20:49:21 -0600

    typeDefs edit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,6 +30,27 @@ const typeDefs = gql`
         projects: [Project]
         tasks: [Task]
     }
+
+    type Query {
+        projects: [Project]
+        project(projectId: ID!): Project
+        tasks: [Task]
+        task(taskId: ID!): Task
+        users: [User]
+        user(userId: ID!): User
+    }
+
+    type Mutation {
+        addProject(name: String!, description: String, startDate: String, endDate: String): Project
+        updateProject(projectId: ID!, name: String, description: String, startDate: String, endDate: String): Project
+        deleteProject(projectId: ID!): Project
+        addTask(name: String!, comment: String, status: String, dueDate: String, project: ID!, assignedTo: ID!): Task
+        updateTask(taskId: ID!, name: String, comment: String, status: String, dueDate: String, project: ID, assignedTo: ID): Task
+        deleteTask(taskId: ID!): Task
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): User
+        updateUser(userId: ID!, firstName: String, lastName: String, email: String, password: String): User
+        deleteUser(userId: ID!): User
+    }
     `;
 
     module.exports = typeDefs;
