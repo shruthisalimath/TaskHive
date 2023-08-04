cf48bb92ccc7c2fedf1110e4b3e2ee9a9b8614a3
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-04 08:57:11 -0600

    SignUp Form:
    -created signup form
    -added validation for the fields

---
+++ client/src/components/SignUp/index.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm(formData);
+
+    if (Object.keys(errors).length === 0) {
+      // Handle form submission here i.e. send data to server and validate password.
+      console.log('Form submitted:', formData);
+    } else {
+      // Throw an alert with each error message
+      Object.values(errors).forEach((error) => alert(error));
+      return;
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (!data.firstName.trim()) {
+      errors.firstName = 'First name is required';
+    }
+
+    if (!data.lastName.trim()) {
+      errors.lastName = 'Last name is required';
+    }
+
+    if (!data.email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!isValidEmail(data.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!data.password.trim()) {
+      errors.password = 'Password is required';
+    }
+
+    if (!data.confirmPassword.trim()) {
+      errors.confirmPassword = 'Confirm password is required';
+    } else if (data.password !== data.confirmPassword) {
+      errors.confirmPassword = 'Passwords do not match';
+    }
+
+    return errors;
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
