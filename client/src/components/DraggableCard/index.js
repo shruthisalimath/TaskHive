ab45de66ba3897590eaec30baf5368c08ce86c3c
Cheryl L <cheryl.loeffler@gmail.com>
2023-08-05 15:57:37 -0600

    modified:   client/src/App.js modified:   client/src/components/DraggableCard/index.js modified:   client/src/components/DroppableZone/index.js modified:   client/src/index.css modified:   client/src/pages/Project.js
     git commit -m "project page
    
     - added and formatted dropzones
     - added and formatted draggable cards
     "
    
     git push origin cheryl

--- client/src/components/DraggableCard/index.js
+++ client/src/components/DraggableCard/index.js
@@ -6,16 +6,18 @@ export default function DraggableCard(props) {
   const {attributes, listeners, setNodeRef, transform} = useDraggable({
     id: props.id,
   });
-  const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-    cursor: 'grabbing'
-  } : 
-  {backgroundColor: 'blue',
-  cursor: 'grab'
-};
+  const style = 
+    transform ? {
+      transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      cursor: 'grabbing'
+    } : 
+    {
+      backgroundColor: 'var(--secondary-color)',
+      cursor: 'grab',
+    };
   
   return (
-    <div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+    <div ref={setNodeRef} className="task-card" style={style} {...listeners} {...attributes}>
       {props.children}
       <h1>Draggable Card</h1>
     </div>
