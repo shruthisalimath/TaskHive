6ef151f9d781d50720333da776e57e713647ebf5
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-06 12:03:19 -0600

    created basic login form component

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm(formData);
+
+    if (Object.keys(errors).length === 0) {
+      // Handle login submission here i.e. send data to server for authentication.
+      console.log('Login submitted:', formData);
+    } else {
+      // Throw an alert with each error message
+      Object.values(errors).forEach((error) => alert(error));
+      return;
+    }
+  };
+
+  const validateForm = (data) => {
+    const errors = {};
+
+    if (!data.email.trim()) {
+      errors.email = 'Email is required';
+    } else if (!isValidEmail(data.email)) {
+      errors.email = 'Invalid email address';
+    }
+
+    if (!data.password.trim()) {
+      errors.password = 'Password is required';
+    }
+
+    return errors;
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
