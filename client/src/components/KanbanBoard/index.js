7b70bb229a3143d70e13ae414fb272a9d3a66431
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-06 15:36:51 -0600

    lotsa changes to test DND components

---
+++ client/src/components/KanbanBoard/index.js
@@ -0,0 +1,73 @@
+import { DndContext, rectIntersection } from "@dnd-kit/core";
+import KanbanLane from "../KanbanLane";
+import AddCard from "../AddCard";
+import { useState } from "react";
+import React from "react";
+
+
+const flexContainerStyle = {
+  display: "flex",
+  flexDirection: "column",
+};
+
+export default function KanbanBoard() {
+  const [todoItems, setTodoItems] = useState([]);
+  const [doneItems, setDoneItems] = useState([]);
+  const [inProgressItems, setInProgressItems] = useState([]);
+  const [uItems, setuItems] = useState([]);
+
+  const addNewCard = (title) => {
+    setuItems([...uItems, { title }]);
+  };
+
+  return (
+    <DndContext
+      collisionDetection={rectIntersection}
+      onDragEnd={(e) => {
+        const container = e.over?.id;
+        const title = e.active.data.current?.title ?? "";
+        const index = e.active.data.current?.index ?? 0;
+        const parent = e.active.data.current?.parent ?? "ToDo";
+
+        if (container === "ToDo") {
+          setTodoItems([...todoItems, { title }]);
+        } else if (container === "Done") {
+          setDoneItems([...doneItems, { title }]);
+        } else if (container === "Unassigned") {
+          setuItems([...uItems, { title }]);
+        } else {
+          setInProgressItems([...inProgressItems, { title }]);
+        }
+
+        if (parent === "ToDo") {
+          setTodoItems([
+            ...todoItems.slice(0, index),
+            ...todoItems.slice(index + 1),
+          ]);
+        } else if (parent === "Done") {
+          setDoneItems([
+            ...doneItems.slice(0, index),
+            ...doneItems.slice(index + 1),
+          ]);
+        } else if (parent === "Unassigned") {
+          setuItems([...uItems.slice(0, index), ...uItems.slice(index + 1)]);
+        } else {
+          setInProgressItems([
+            ...inProgressItems.slice(0, index),
+            ...inProgressItems.slice(index + 1),
+          ]);
+        }
+      }}
+    >
+      <div style={flexContainerStyle}>
+        <AddCard addCard={addNewCard} />
+        <div style={{ display: "flex" }}>
+          <KanbanLane title="ToDo" items={todoItems} />
+          <KanbanLane title="In Progress" items={inProgressItems} />
+          <KanbanLane title="Done" items={doneItems} />
+          <KanbanLane title="Unassigned" items={uItems} />
+        </div>
+      </div>
+    </DndContext>
+  );
+}
