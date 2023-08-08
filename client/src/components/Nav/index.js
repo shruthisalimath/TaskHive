17ff12f833efddaa6a4dc6d6c3d620a6601c4fd4
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-08-07 20:00:04 -0600

    added logout functionality

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { useNavigate } from "react-router-dom";
 
-const Nav = () => {
+function NavBar() {
+  function logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem("id_token");
+    // this will reload the page and reset the state of the application
+    navigate("/");
+  }
+  const navigate = useNavigate();
+
   return (
-    <div className='nav-bar'>
-        <Link to='/'> Profile </Link>
-        <Link to='/'> Logout </Link>
+    <div className="nav-bar">
+      <Link to="/home">Profile</Link>
+      <Link to="/" onClick={logout}>
+        Logout
+      </Link>
     </div>
   );
 }
 
-export default Nav;
+export default NavBar;
