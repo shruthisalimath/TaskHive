6e372e8c8c21a616e9e9fe8509c1ecbe94a00741
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-08 21:50:43 -0600

    DeleteBtn:
    -Delete Button added to TaskCard
    -Modified files to allow delete button to work

--- client/src/components/TaskCard/index.js
+++ client/src/components/TaskCard/index.js
@@ -2,19 +2,15 @@ import React from "react";
 import { useDraggable } from "@dnd-kit/core";
 import { CSS } from "@dnd-kit/utilities";
 import { useMutation } from "@apollo/client";
-import { UPDATE_TASK } from "../../utils/mutations";
+import { UPDATE_TASK, REMOVE_TASK } from "../../utils/mutations";
 
-const TaskCard = ({ task, index, parent }) => {
+const TaskCard = ({ task }) => {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: task._id,
-    data: {
-      task,
-      index,
-      parent,
-    },
   });
 
   const [updateTask] = useMutation(UPDATE_TASK);
+  const [removeTask] = useMutation(REMOVE_TASK);
 
   const style = {
     padding: "8px",
@@ -40,15 +36,27 @@ const TaskCard = ({ task, index, parent }) => {
     }
   };
 
+  const handleDeleteTask = async () => {
+    try {
+      await removeTask({
+        variables: {
+          taskId: task._id,
+        },
+      });
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
+  };
+
   return (
     <div
       style={style}
-      {...listeners}
       {...attributes}
+      {...listeners}
       ref={setNodeRef}
       onDragEnd={(event) => {
         const overLane = event.over ? event.over.id : null;
-        if (overLane !== parent) {
+        if (overLane) {
           handleTaskDrop(overLane);
         }
       }}
@@ -57,6 +65,7 @@ const TaskCard = ({ task, index, parent }) => {
       <p>Comment: {task.comment}</p>
       <p>Status: {task.status}</p>
       <p>Due Date: {task.dueDate}</p>
+      <button onClick={handleDeleteTask}>Delete</button>
     </div>
   );
 };
