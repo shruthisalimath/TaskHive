ffebb9bf647c11f60760268522e4616c94799eb3
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-08-10 17:57:23 -0600

    added * to path for production in server.js

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+};
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
