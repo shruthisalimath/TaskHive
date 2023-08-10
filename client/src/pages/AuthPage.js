d52273634a561cdf8cdd30a9fee4017840ea78a6
Cheryl L <cheryl.loeffler@gmail.com>
2023-08-10 16:09:39 -0600

    finished css

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -12,12 +12,16 @@ function AuthPage() {
 
   return (
     <div className='auth-page'>
+      <div className='logo-container'>
       {/* <h1>Welcome to Our App</h1> */}
       <img className='logo-lg' src={logo} alt='TaskHive Logo'/>
-      <p onClick={toggleSignUp} style={{ cursor: 'pointer', textDecoration: 'underline' }}>
-        {showSignUp ? 'Back to Login' : 'Sign Up'}
-      </p>
+      </div>
+      <div className='form-container'>
       {showSignUp ? <SignUp /> : <Login />}
+      <p className="sign-login-btn"onClick={toggleSignUp} style={{ cursor: 'pointer' }}>
+        {showSignUp ? 'Back to Login' : 'New here? Sign up'}
+      </p>
+      </div>
     </div>
   );
 }
