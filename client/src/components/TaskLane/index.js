51777224924bbf6037d4261ac0340a7c8dfc7384
josh4got <125082025+josh4got@users.noreply.github.com>
2023-08-10 17:02:11 -0600

    CLEAN CODE:
    -removed unused components
    -removed unused dependencies
    -added some code comments

--- client/src/components/TaskLane/index.js
+++ client/src/components/TaskLane/index.js
@@ -1,33 +1,23 @@
 import React from "react";
-import { useDroppable } from "@dnd-kit/core";
 import TaskCard from "../TaskCard";
-import { useMutation } from "@apollo/client";
-import { UPDATE_TASK } from "../../utils/mutations";
 
 function TaskLane({ title, tasks }) {
-    console.log("tasks", tasks);
-    console.log("title", title);
-  const { setNodeRef, isOver, isOverContainer } = useDroppable({
-    id: title,
-  });
 
-  const [updateTask, { loading, error, data: updateTaskData }] = useMutation(UPDATE_TASK);
 
+
+  // styling for the lanes
   const laneStyle = {
     padding: "5px",
     flexDirection: "column",
     minHeight: "10rem",
-    border: isOver && isOverContainer ? "2px dashed green" : "none",
   };
 
-  const laneContentStyle = {
-     
-  };
 
+  // return statement renders TaskCards into the corresponding lane
   return (
     <div className="lane" style={laneStyle}>
       <h2 >{title}</h2>
-      <div ref={setNodeRef} style={laneContentStyle}>
+      <div>
         {tasks.map((task, key) => (
           <TaskCard task={task} key={task._id} index={key} parent={title} />
         ))}
